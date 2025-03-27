794822312a120edea80055198dbfc8629f43ea3b
danpetraccaro <danpetraccaro@gmail.com>
2025-03-27 22:31:45 +1030

    compoent

---
+++ src/app/food-audit/food-audit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-audit',
+  imports: [],
+  templateUrl: './food-audit.component.html',
+  styleUrl: './food-audit.component.scss'
+})
+export class FoodAuditComponent {
+
+}
---
+++ src/app/food-surveillance/food-surveillance.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-surveillance',
+  imports: [],
+  templateUrl: './food-surveillance.component.html',
+  styleUrl: './food-surveillance.component.scss'
+})
+export class FoodSurveillanceComponent {
+
+}
