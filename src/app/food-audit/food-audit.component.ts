4cae7b7ce5ffd998c373be131f2de3eff37c8919
null <null>
2025-03-31 09:59:31 +1030

    working

--- src/app/food-audit/food-audit.component.ts
+++ src/app/food-audit/food-audit.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterModule } from '@angular/router';
 import { ToolbarComponent } from '../toolbar/toolbar.component'; // Adjust the path as needed
+import { MatFormField } from '@angular/material/form-field';
 
 // Import Angular Material modules
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+
 
 
 @Component({
@@ -19,7 +22,9 @@ import { MatIconModule } from '@angular/material/icon';
     ToolbarComponent,
     MatSidenavModule, // Import MatSidenavModule here
     MatListModule,
-    MatIconModule
+    MatIconModule,
+    MatFormField,
+    MatCardModule
   ],
   templateUrl: './food-audit.component.html',
   styleUrls: ['./food-audit.component.scss']
