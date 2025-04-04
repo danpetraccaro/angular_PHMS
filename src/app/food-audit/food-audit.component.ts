cb087168322833ef16126387ba25108d98aea94b
danpetraccaro <danpetraccaro@gmail.com>
2025-04-04 12:59:04 +1030

    working premise

--- src/app/food-audit/food-audit.component.ts
+++ src/app/food-audit/food-audit.component.ts
@@ -1,10 +1,38 @@
-import { CommonModule } from '@angular/common';
+
+
+import { ActivatedRoute,} from '@angular/router';
+import { CommonModule, getLocaleDateFormat } from '@angular/common';
 import { Component } from '@angular/core';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
+import { RouterLink, RouterModule } from '@angular/router';
+import { ToolbarComponent } from '../toolbar/toolbar.component'; // Adjust the path as needed
+// Import Angular Material modules
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { TPremises } from '../types/premises';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-food-audit',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule,
+    RouterModule,
+    RouterLink,
+    ToolbarComponent,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatCardModule,
+    MatTableModule,
+    MatListModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule],
   templateUrl: './food-audit.component.html',
   styleUrl: './food-audit.component.scss'
 
