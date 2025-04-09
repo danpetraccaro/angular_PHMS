5a0ae1b88a68cee2621df68e8009de498f7de529
danpetraccaro <danpetraccaro@gmail.com>
2025-04-09 16:55:42 +0930

    working

--- src/app/inspection/inspection.component.ts
+++ src/app/inspection/inspection.component.ts
@@ -1,11 +1,33 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, inject } from '@angular/core';
-import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { ActivatedRoute, RouterLink, RouterModule } from '@angular/router';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
 
 @Component({
   selector: 'app-inspection',
-  imports: [CommonModule,
-    RouterModule],
+  imports: [    CommonModule,
+    InspectionComponent,
+    RouterModule,
+    RouterLink,
+    ToolbarComponent,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatCardModule,
+    MatTableModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule],
   templateUrl: './inspection.component.html',
   styleUrl: './inspection.component.scss'
 })
